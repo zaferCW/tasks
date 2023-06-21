7fbafb2de7539f591dbf6cf11265c264f391685b
zaferCW <zafer@clarusway.com>
2023-06-21 23:12:40 +0300

    task 12 added

---
+++ src/task12/Task06.java
@@ -0,0 +1,11 @@
+package task12;
+
+public class Task06 {
+    public static void main(String[] args) {
+        // Task-> verilen bir int arrayi elemanlrını buyukten kucuge siralayip return eden METHOD create ediniz
+
+
+    }
+
+
+}
