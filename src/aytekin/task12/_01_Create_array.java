1f9377899959b480abd50dfc80bfb8013b2e9550
AYTEKİN KAPLAN <aoniki2018@gmail.com>
2023-06-22 23:45:41 +0300

    Selam

--- src/aytekin/task12/_01_Create_array.java
+++ src/aytekin/task12/_01_Create_array.java
@@ -1,4 +1,4 @@
-package task12;
+package aytekin.task12;
 
 public class _01_Create_array {
 
@@ -10,7 +10,7 @@ public class _01_Create_array {
          */
 
         //Kodu aşağıya yazınız.
-        System.out.println("Selam");
+
 
     }
 }
