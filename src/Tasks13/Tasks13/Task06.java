66bdd5258feded98134b136d5d91791afc9c04b7
ali <alig90901@gmail.com>
2023-07-02 21:32:24 +0100

    degisiklikler

--- src/Tasks13/Tasks13/Task06.java
+++ src/Tasks13/Tasks13/Task06.java
@@ -1,16 +1,43 @@
 package Tasks13.Tasks13;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task06 {
-    public static void main(String[] args) {
          /* TASK :
         Kullanıcıdan alacağınız 6 elemanlı bir dizinin
         sadece tek elemanlarını ayrı diziye bir metodda atayarak
          yazdırınız.
 
         */
+         public static List<Integer> tekElemanlariAyir(List<Integer> liste) {
+             List<Integer> tekler = new ArrayList<>();
+             for (int eleman : liste) {
+                 if (eleman % 2 == 1) {
+                     tekler.add(eleman);
+                 }
+             }
+             return tekler;
+         }
 
+    public static void main(String[] args) {
+        List<Integer> dizi = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
 
-    }//main sonu
+        for (int i = 0; i < 6; i++) {
+            System.out.print("Dizinin " + (i + 1) + ". elemanını girin: ");
+            int eleman = scanner.nextInt();
+            dizi.add(eleman);
+        }
 
-
+        List<Integer> teklerDizisi = tekElemanlariAyir(dizi);
+        System.out.println("Tek elemanlar: " + teklerDizisi);
+    }
 }
+
+
+
+
+
+
--- src/Tasks13/Tasks13/Task07.java
+++ src/Tasks13/Tasks13/Task07.java
@@ -1,19 +1,34 @@
 package Tasks13.Tasks13;
 
 public class Task07 {
+
+    /* TASK :
+     * 10 elamanli bir list olusturun. Ardindan dizinin 3. elemani ile
+     * 8. elemaninin yerlerini degistirin.
+     *
+     * ORNEK:
+     *
+     * INPUT : String[] isimler={"Umit","Emin","Kemal","Kerem","Taylan","Orhan","Sinan","Furkan","Ahmet","Ali"};
+     * OUTPUT : String[] isimler={"Umit","Emin","Furkan","Kerem","Taylan","Orhan","Sinan","Kemal","Ahmet","Ali"};
+     */
     public static void main(String[] args) {
-        /* TASK :
-         * 10 elamanli bir list olusturun. Ardindan dizinin 3. elemani ile
-         * 8. elemaninin yerlerini degistirin.
-         *
-         * ORNEK:
-         *
-         * INPUT : String[] isimler={"Umit","Emin","Kemal","Kerem","Taylan","Orhan","Sinan","Furkan","Ahmet","Ali"};
-         * OUTPUT : String[] isimler={"Umit","Emin","Furkan","Kerem","Taylan","Orhan","Sinan","Kemal","Ahmet","Ali"};
-         */
+        String[] isimler = {"Umit", "Emin", "Kemal", "Kerem", "Taylan", "Orhan", "Sinan", "Furkan", "Ahmet", "Ali"};
 
+        // Dizinin 3. elemanı ile 8. elemanının yerlerini değiştirme
+        String temp = isimler[2]; // 3. elemanı geçici bir değişkene kaydetme
+        isimler[2] = isimler[7]; // 3. elemanın yerine 8. elemanı atama
+        isimler[7] = temp; // 8. elemanın yerine geçici değişkendeki değeri atama
 
-
-
+        // Sonucu ekrana yazdırma
+        System.out.println("Dizinin 3. elemanı ile 8. elemanının yerleri değiştirildi:");
+        for (String isim : isimler) {
+            System.out.print(isim + " ");
+        }
     }
 }
+
+
+
+
+
+
