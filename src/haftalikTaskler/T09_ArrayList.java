7a2a8d21664adf5f8ecddc00d0cb37c68e2c4755
zaferCW <zafer@clarusway.com>
2023-07-03 13:21:28 +0300

    hyaftalik taskler ve task13 eklendi

---
+++ src/haftalikTaskler/T09_ArrayList.java
@@ -0,0 +1,20 @@
+package haftalikTaskler;
+
+public class T09_ArrayList {
+
+    /*
+      Task:
+
+    Create 2D ArrayList which can store String ArrayLists
+    Create 3 ArrayLists which are Employees, Employers, Companies
+    Store this 3 ArrayList in 2D ArrayList
+    String data type ArrayList'leri depolayabilen 2D ArrayList create ediniz
+    Çalışanlar, İşverenler, Şirketler olmak üzere 3 ArrayListesi oluşturun
+    Bu 3 ArrayList'i 2D ArrayList'te saklayın
+     */
+
+    public static void main(String[] args) {
+
+
+    }
+}
