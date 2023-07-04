1290fe1a51529f3d65661f4ed08aea0bae4aec93
Safvet <believeandendeavour@gmail.com>
2023-07-04 23:10:48 +0300

    hello

---
+++ src/safvet/Tasks13/Tasks13/_09_2dArray_with_arraylis.java
@@ -0,0 +1,39 @@
+package safvet.Tasks13.Tasks13;
+
+import java.util.ArrayList;
+
+public class _09_2dArray_with_arraylis {
+
+    /*
+    Bir tane 2d array [][]   oluşturun ve elemanları =  [  [ 1, 2, 3 ],  [ 4, 5, 6 ],  [ 7, 8, 9 ] ]
+    Store all the elements in one arraylist and print the arraylist
+    Tüm elemanları bir arrayListe yükleyin ve bu arrayList'i yazdırın.
+    Örnek:
+    Girdi:
+    [
+     [ 1, 2, 3 ],
+     [ 4, 5, 6 ],
+     [ 7, 8, 9 ]
+    ]
+    Çıktı: [1,2,3,4,5,6,7,8,9]
+     */
+
+    public static void main(String[] args) {
+        int[][] array = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+        ArrayList<Integer> arrayList = flattenArray(array);
+        System.out.println("ArrayList: " + arrayList);
+    }
+
+    public static ArrayList<Integer> flattenArray(int[][] array) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        for (int[] row : array) {
+            for (int element : row) {
+                arrayList.add(element);
+            }
+        }
+
+        return arrayList;
+    }
+}
---
+++ src/safvet/task12/Task05.java
@@ -0,0 +1,24 @@
+package safvet.task12;
+
+import java.util.Scanner;
+
+public class Task05 {
+
+
+    public static void main(String[] args) {
+        // Task-> girilen int değeri tersten print eden code create ediniz.
+        Scanner input = new Scanner(System.in) ;
+
+        System.out.println("Bir deger giriniz : ");
+        int numara = input.nextInt();
+
+        String ters = new StringBuilder(String.valueOf(numara)).reverse().toString();
+        System.out.println("tersten hali : "+ ters);
+
+
+
+
+
+    }
+
+}
