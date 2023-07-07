5e1622de956828434fd7b13dd995c668ecb950cc
ali <alig90901@gmail.com>
2023-07-07 21:53:11 +0100

    ozeltaskler

---
+++ src/OzelTaskler/T04_AnagramKontrol.java
@@ -0,0 +1,24 @@
+package OzelTaskler;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class T04_AnagramKontrol {
+
+    public static void main(String[] args) {
+		/*
+		 * Anagram, bir kelimenin harflerinden baska bir kelime olusturmaya denir.
+			Kullanıcıdan alınacak  iki kelimenin Anagram kontrolu yapan  Java code create ediniz
+			isAnagram("listen", "Silent") ==> true
+		 */
+
+
+    }
+
+
+
+
+
+
+
+}
---
+++ src/OzelTaskler/T08_ArrayList.java
@@ -0,0 +1,15 @@
+package OzelTaskler;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class T08_ArrayList {
+    public static void main(String[] args) {
+        /*
+        Bir tamsayı listesinde en yakın 2 tamsayıyı bulunuz.
+         Örnek: (12, 31, 15, 32, 54) ==> Çıktı 31 ve 32'tür
+         */
+
+
+    }}
