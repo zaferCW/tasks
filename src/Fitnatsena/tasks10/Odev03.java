0e5723d76bebc40be6a4776275e3634eb5a1caef
fitnat-c <catpinarsena@gmail.com>
2023-07-15 13:25:56 +0300

    ozel taks eklendi

--- src/Fitnatsena/tasks10/Odev03.java
+++ src/Fitnatsena/tasks10/Odev03.java
@@ -18,7 +18,27 @@ public class Odev03 {
 
         int sayi= str.nextInt();
 
+        System.out.println("getAsalMı(sayi) = " + getAsalMı(sayi));
 
+
+    }
+
+    private static boolean getAsalMı(int sayi) {
+        boolean asal=true;
+        if (sayi<=1){
+            asal=false;
+        }
+        for (int i = 2; i <=Math.sqrt(sayi) ; i++) {
+            //System.out.println("Math.sqrt(sayi) = " + Math.sqrt(sayi));
+            System.out.println("i = " + i);
+            if (sayi % i ==0){
+                asal=false;
+                break;
+
+            }
+
+        }
+return asal;
     }
 
 
--- src/Fitnatsena/tasks10/Task08.java
+++ src/Fitnatsena/tasks10/Task08.java
@@ -1,5 +1,7 @@
 package Fitnatsena.tasks10;
 
+import java.util.Scanner;
+
 public class Task08 {
 
     public static void main(String[] args) {
@@ -14,7 +16,20 @@ public class Task08 {
     Beklenen Çıktı:
     false
     */
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bir yıl giriniz: ");
+        int yil = input.nextInt();
+        boolean sonuc;
 
+
+         if ((yil % 4 == 0 && yil % 100 != 0) || (yil % 400 == 0)) {
+             sonuc=true;
+
+         }else{
+
+         sonuc=false;}
+
+        System.out.println("sonuc = " + sonuc);
 
 
     }
