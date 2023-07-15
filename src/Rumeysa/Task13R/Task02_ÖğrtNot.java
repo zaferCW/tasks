360d79550a316b8a64c640ea9efcc2049f9240e9
Rumeysa <rumeysaarsvn@gmail.com>
2023-07-15 15:22:39 +0200

    tasks eklendi

--- src/Rumeysa/Task13R/Task02_ÖğrtNot.java
+++ src/Rumeysa/Task13R/Task02_ÖğrtNot.java
@@ -1,7 +1,12 @@
 package Rumeysa.Task13R;
 
-
+import java.util.ArrayList;
+import java.util.Scanner;
 public class Task02_ÖğrtNot {
+    static Scanner input = new Scanner(System.in);
+    static double toplam ;
+    static int count ;
+    static ArrayList<Double> notlar = new ArrayList<>();
     public static void main(String[] args) {
           /* TASK :
          Bir öğretmenden girmek istediği kadar notu alınız, ve
@@ -9,8 +14,42 @@ public class Task02_ÖğrtNot {
 
          */
 
+        notGirisi();
 
+    }
 
+    private static void notGirisi() {
+        System.out.println("kac adet not girmek istiyorsunuz");
+        int adet = input.nextInt();
 
+        double toplam = 0;
+        int count = 0;
+        while (count < adet){
+            System.out.println(count +1 + ". notu giriniz");
+            double not = input.nextDouble();
+            toplam += not;
+            notlar.add(not);
+            count++;
+        }
+        double ort = ortalamaHesapla(toplam, count);
+        System.out.println("ort = " + ort);
+
+        System.out.println("ortalamayi Gecenler = " + ortalamayiGecenler(notlar, ort));
+
+    }
+
+    private static int ortalamayiGecenler(ArrayList<Double> notlar, double ort) {
+        int gecenler = 0;
+        for (double each : notlar) {
+            if (each > ort){
+                gecenler ++;
+            }
+
+        }
+        return gecenler;
+    }
+
+    private static double ortalamaHesapla(double x, int y) {
+        return x/y;
     }
 }
